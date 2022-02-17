ea78c879d39c5e01cd41c1e43392aa674c08e53d
Renan <86449025+iRnx@users.noreply.github.com>
2022-02-17 13:29:46 -0300

    Condição simples

--- ex007/ex007.js
+++ ex007/ex007.js
@@ -1,1 +1,8 @@
+var velocidade = 60
+
+console.log(`A velocidade do seu carro é ${velocidade}km/h`)
+if(velocidade > 60) { // Condição simples!
+    console.log(`Vc passou da velocidade permitida...`)
+}
+console.log(`Dirija sempre com cinto de segurança!`)
 
