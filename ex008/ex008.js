f51f4e69d59a4ae9096ece6ecb2c1e64e7957180
Renan <86449025+iRnx@users.noreply.github.com>
2022-02-22 13:09:00 -0300

    condição simples

--- ex008/ex008.js
+++ ex008/ex008.js
@@ -1,1 +1,8 @@
-
+var país = 'EUA'
+console.log(`Vivendo em ${país}`)
+if(país == 'eua'.toUpperCase()){
+    console.log(`boa somos dos Estados Unidos da América`)
+}
+else {
+    console.log(`Não somos dos eua`)
+}
