36f092774f3ceac55006f10a3dc07f429d14b236
Renan <86449025+iRnx@users.noreply.github.com>
2022-02-23 13:13:01 -0300

    Verificando se a pessoa nasceu nos EUA

--- ex009/ex009.js
+++ ex009/ex009.js
@@ -1,1 +1,8 @@
-
+var país = 'EUA'
+console.log(`Vivendo em ${país}`)
+if(país == 'eua'.toUpperCase()){
+    console.log('Você mora no Melhor país do mundo!! Estados Unidos da América')
+}
+else {
+    console.log('Você não mora no melhor país do mundo!!')
+}
