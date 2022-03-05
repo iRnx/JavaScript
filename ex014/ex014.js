6cd07604f6231bc3b27d7d6593f546647423f407
Renan <86449025+iRnx@users.noreply.github.com>
2022-03-05 12:07:43 -0300

    Usando switch case

--- ex014/ex014.js
+++ ex014/ex014.js
@@ -1,1 +1,51 @@
+var agora = new Date()
+var diasem = agora.getDay()
 
+/* Para o get dai é assim que ele pega qual é o dia ↓↓
+
+    0 = Domingo
+    1 = Segunda
+    2 = Terça
+    3 = Quarta
+    4 = Quinta
+    5 = Sexta
+    6 = Sábado
+*/
+
+console.log(diasem)
+
+switch(diasem) {
+    case 0:
+        console.log('Domingo')
+        break
+
+    case 1:
+        console.log('Segunda')
+        break
+
+    case 2:
+        console.log('Terça')
+        break
+
+    case 3:
+        console.log('Quarta')
+        break
+
+    case 4:
+        console.log('Quinta')
+        break
+
+    case 5:
+        console.log('Sexta')
+        break
+
+    case 6:
+        console.log('Sábado')
+        break
+
+    default:
+        console.log('[ERRO] Dia inválido')
+        break
+}
+
+// O break é OBRIGATÓRIO dentro do swith case
