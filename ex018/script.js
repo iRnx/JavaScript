e38f79c1c01fb72d59be1f0abc5a4a82a11cdaab
Renan <86449025+iRnx@users.noreply.github.com>
2022-03-09 15:34:33 -0300

    while e do while

--- ex018/script.js
+++ ex018/script.js
@@ -1,11 +1,11 @@
-// primeiro faz a estrutura logica e depois executa o bloco
+// primeiro faz a estrutura logica e depois executa o bloco //
 c = 1
 while(c <= 6){
     console.log('GG fdp')
     c++
 }
 
-// Executa primeiro o bloco e depois faz a estrutura logica
+// Executa primeiro o bloco e depois faz a estrutura logica //
 do {
     console.log('caraai GG')
     c++
