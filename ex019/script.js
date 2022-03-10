e8730a2effffec91759bebf42739e6d3ffbeb483
Renan <86449025+iRnx@users.noreply.github.com>
2022-03-10 13:18:53 -0300

    começando com for

--- ex019/script.js
+++ ex019/script.js
@@ -1,1 +1,13 @@
+/*
+console.log('VAI COMEÇAR...')
+for(c = 1; c <= 5; c++){
+    console.log(c)
+}
+console.log('FIM!')
+*/
 
+console.log('VAI COMEÇAR...')
+for(var c = 1; c <= 5; c++){
+    console.log(c)
+}
+console.log('FIM!')
