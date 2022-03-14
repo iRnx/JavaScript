a4de8c5a85062917ae11f7e2078453e585f6bab9
Renan <86449025+iRnx@users.noreply.github.com>
2022-03-14 00:26:03 -0300

    Array, usando os dois tipos de for

--- ex023/script.js
+++ ex023/script.js
@@ -1,1 +1,9 @@
+var num = [6, 2, 1, 9, 8]
 
+for(pos = 0; pos < num.length; pos++){
+    console.log(`A posição ${pos} tem o valor ${num[pos]}`)
+}
+
+for(pos in num){
+    console.log(`A posição ${pos} tem o valor ${num[pos]}`)
+}
